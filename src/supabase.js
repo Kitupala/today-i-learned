57f664b919632c5a4e18b43076343f89f684cb69
Kitupala <kimmo.tuupanen@gmail.com>
2024-09-29 20:56:22 +0300

    Change supabase settings

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vrzmuouatcyfwenqlhys.supabase.co";
+const supabaseUrl = "https://zhjkbdabdimorfuzoedm.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyem11b3VhdGN5ZndlbnFsaHlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ5MjY4MjAsImV4cCI6MTk5MDUwMjgyMH0.tqHFkGPiD5pCqOZ_GGr77IyxUQWiNL7nYzVJu8PZoSM";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoamtiZGFiZGltb3JmdXpvZWRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0MzAwMDgsImV4cCI6MjA0MzAwNjAwOH0.bB_0yUkAaxUOFSFa1Quw_Lklg61b0cgMfLNXU5gHniM";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
